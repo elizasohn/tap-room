9ce0254a3347050f3c306e93f7ef5e03c07a031b
Eliza Sohn <eliza.sohn@gmail.com>
2020-02-21 13:03:37 -0800

    move hard coded keglist to constructor in app

--- src/App.js
+++ src/App.js
@@ -14,7 +14,68 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      masterKegList: []
+      masterKegList: [
+        {
+          name: "Pils",
+          brand: "Heater Allen Brewing",
+          price: "$7.00",
+          alcoholContent: "4.8%"
+        },
+        {
+          name: "HUB Lager",
+          brand: "Hopworks Urban Brewery",
+          price: "$6.50",
+          alcoholContent: "5.1%"
+        },
+        {
+          name: "Pilsner",
+          brand: "Breakside Brewing Co.",
+          price: "$6.50",
+          alcoholContent: "5.3%"
+        },
+        {
+          name: "Session Lager",
+          brand: "Full Sail Brewing Co.",
+          price: "$6.00",
+          alcoholContent: "5.1%"
+        },
+        {
+          name: "Kölsch",
+          brand: "Occidental Brewing Co.",
+          price: "$6.50",
+          alcoholContent: "4.4%"
+        },
+        {
+          name: "Sterling Pils",
+          brand: "Ninkasi Brewing Co.",
+          price: "$7.00",
+          alcoholContent: "5.1%"
+        },
+        {
+          name: "Omission Lager",
+          brand: "Widmer Brothers Brewing",
+          price: "$7.00",
+          alcoholContent: "4.6%"
+        },
+        {
+          name: "The Vaporizer",
+          brand: "Double Mountain Brewery",
+          price: "$7.50",
+          alcoholContent: "6%"
+        },
+        {
+          name: "Watershed IPA",
+          brand: "Oakshire Brewing Co.",
+          price: "$7.00",
+          alcoholContent: "7.1%"
+        },
+        {
+          name: "RPM",
+          brand: "Boneyard Brewing Co.",
+          price: "$6.50",
+          alcoholContent: "7%"
+        }
+      ]
     };
     this.handleAddingNewKegToList = this.handleAddingNewKegToList.bind(this);
   }

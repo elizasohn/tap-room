d96298c30cb762efbf8401d2dd4409781ba7a17a
Eliza Sohn <eliza.sohn@gmail.com>
2020-02-21 10:36:06 -0800

    add new keg form

---
+++ src/components/NewKegControl.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+function NewKegForm(props){
+  let _name = null;
+  let _brand = null;
+  let _price = null;
+  let _alcoholContent = null;
+
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    props.onNewKegCreation(
+      {name: _name.value,
+      brand: _brand.value,
+      price: _price.value,
+      alcoholContent: _alcoholContent.value,});
+    _name.value = '';
+    _brand.value ='';
+    _price.value = '';
+    _alcoholContent.value = '';
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleNewKegFormSubmission}>
+        <input
+          type='text'
+          id='name'
+          placeholder='Keg Name'
+          ref={(input) => {_name = input;}}/>
+        <input
+          type='text'
+          id='brand'
+          placeholder='Brand'
+          ref={(input) => {_brand = input;}}/>
+        <input
+          type='text'
+          id='price'
+          placeholder='Price'
+          ref={(input) => {_price = input;}}/>
+        <input
+          type='text'
+          id='alcoholContent'
+          placeholder='Alcohol Content'
+          ref={(input) => {_alcoholContent = input;}}/>
+        <button type='submit'>Add Keg</button>
+      </form>
+    </div>
+  );
+}
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+};
+
+export default NewKegForm;

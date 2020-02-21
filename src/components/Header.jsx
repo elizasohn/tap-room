17f37cc279429bdc6de388e5a681f3ee22004c80
Eliza Sohn <eliza.sohn@gmail.com>
2020-02-21 14:02:46 -0800

    add link to new keg in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,7 +29,12 @@ function Header(){
   return (
     <div style = {headerStyle}>
       <div style = {links} className="links">
-      <h2><Link to="/" style={{ color: '#FFF' }}>HOME</Link> | <Link to="/keglist" style={{ color: '#FFF' }}>KEGS ON TAP</Link> | <Link to="/asdfasdf" style={{ color: '#FFF' }}>COOL ERROR MESSAGE</Link></h2>
+      <h2>
+        <Link to="/" style={{ color: '#FFF' }}>HOME</Link> |
+        <Link to="/keglist" style={{ color: '#FFF' }}>KEGS ON TAP</Link> |
+        <Link to="/newkeg" style={{ color: '#FFF' }}>ADD A KEG</Link> | 
+        <Link to="/asdfasdf" style={{ color: '#FFF' }}>ERROR</Link>
+      </h2>
       </div>
       <hr/>
     </div>

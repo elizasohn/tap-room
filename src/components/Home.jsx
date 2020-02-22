d512588ae915e31b3da8287e4f58effff2611034
Eliza Sohn <eliza.sohn@gmail.com>
2020-02-21 16:20:34 -0800

    adjust styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,17 @@ import React from "react";
 
 function Home(){
   var homeStyle = {
-  backgroundImage: "url('beerhall.jpg')",
-  backgroundRepeat: 'no-repeat',
-  backgroundSize: 'cover',
-  textAlign: 'center',
+    display:'flex',
+    flexWrap: 'wrap',
+    justifyContent:'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    padding: '4vh',
+    overflow: 'visible',
+    borderRadius: '4vh',
+    margin:'20px',
+    fontSize: '14px',
+    textAlign: 'left',
+    contentAlign: 'center',
   }
 
 return (
